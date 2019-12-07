de7a16cbd70762cd6d6389e2757480d44088ff95
Ihar Amelyaniuk <iamelyaniuk@travix.com>
2019-12-07 17:04:31 +0300

    Reversestring done

--- reversestring/index.js
+++ reversestring/index.js
@@ -6,6 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// Solution #1
+function reverse(str) {
+  return str.split('').reverse().join('');
+}
+
+// Solution #2
+// function reverse(str) {
+//   let reversedStr = '';
+
+//   for (let character of str) {
+//     reversedStr = character + reversedStr;
+//   }
+
+//   return reversedStr;
+// }
+
+// Solution #3
+// function reverse(str) {
+//   return str.split('').reduce((acc, current) => current + acc, '');
+// }
 
 module.exports = reverse;

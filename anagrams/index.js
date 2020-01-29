3348f8486386b6a0fff97aacc2d264087b444a6e
Ihar Amelyaniuk <iamelyaniuk@travix.com>
2020-01-29 23:00:38 +0300

    Add some stuff

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,6 +8,44 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+
+// function buildMap(str) {
+//   const charMap = {};
+
+//   for (let char of str.replace(/[\W]/g, '').toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+
+//   return charMap;
+// }
+
+// function anagrams(stringA, stringB) {
+//   const charMapA = buildMap(stringA);
+//   const charMapB = buildMap(stringB);
+  
+//   if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+//     return false;
+//   }
+
+//   for (let key in charMapA) {
+//     if (charMapA[key] !== charMapB[key]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+
+function cleanStr(str) {
+  return str.replace(/[\W]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
+}
+
+function anagrams(stringA, stringB) {
+  return cleanStr(stringA) === cleanStr(stringB);
+}
 
 module.exports = anagrams;
--- pyramid/index.js
+++ pyramid/index.js
@@ -13,7 +13,54 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//   pyramid(4)
+//      '   #   '
+//      '  ###  '
+//      ' ##### '
+//      '#######'
 
-function pyramid(n) {}
+
+// function pyramid(n) {
+//   const width = 2 * n - 1;
+//   const middle = Math.floor(width / 2);
+//   for (let row = 0; row < n; row++) {
+//     let str = '';
+
+//     for (let column = 0; column < width; column++) {
+//       if (column <= middle + row && column >= middle - row) {
+//         str += '#';
+//       } else {
+//         str += ' ';
+//       }
+//     }
+
+//     console.log(str);
+//   }
+// }
+
+function pyramid(n, row = 0, str = '') {
+  if (row === n) {
+    return;
+  }
+
+  const width = 2 * n - 1;
+
+  if (str.length === width) {
+    console.log(str);
+    return pyramid(n, row + 1)
+  }
+
+  const middle = Math.floor(width / 2);
+  let temporary;
+
+  if (str.length <= middle + row && str.length >= middle - row) {
+    temporary = '#';
+  } else {
+    temporary = ' ';
+  }
+
+  pyramid(n, row, str + temporary);
+
+}
 
 module.exports = pyramid;

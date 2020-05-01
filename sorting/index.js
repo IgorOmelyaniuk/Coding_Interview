7c90ff64347b98cf5a4b7379150fd5fe444f2a1c
Ihar Amelyaniuk <iamelyaniuk@travix.com>
2020-05-01 15:15:10 +0300

    Solve sorting

--- sorting/index.js
+++ sorting/index.js
@@ -2,19 +2,63 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < (arr.length - i - 1); j++) {
+      if (arr[j] > arr[j+1]) {
+        let swap = arr[j+1];
+        arr[j+1] = arr[j];
+        arr[j] = swap;
+      }
+    }
+  }
 
+  return arr;
 }
 
 function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
 
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[indexOfMin] > arr[j]) {
+        indexOfMin = j;
+      }
+    }
+
+    if (indexOfMin !== i) {
+      let swap = arr[i];
+      arr[i] = arr[indexOfMin];
+      arr[indexOfMin] = swap;
+    }
+  }
+
+  return arr;
 }
 
 function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
 
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
+  const result = [];
 
+  while(left.length && right.length) {
+    if (left[0] < right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+
+  return [...result, ...left, ...right]
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
